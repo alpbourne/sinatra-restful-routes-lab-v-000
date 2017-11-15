f3c220bd97e4f8953f6e435c5ebf31187f79eaf5
alpbourne <alpbourne@gmail.com>
2017-11-15 20:43:28 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,6 @@ class ApplicationController < Sinatra::Base
     set :public_folder, 'public'
     set :views, 'app/views'
   end
-  get '/' do
-    erb :index
-  end
 
   get '/recipes/new' do
    erb :new
@@ -21,12 +18,12 @@ class ApplicationController < Sinatra::Base
    redirect to "/recipes/#{@recipe.id}"
  end
 
- get 'recipes/:id' do
+ get '/recipes/:id' do
    @recipe = Recipe.find_by_id(params[:id])
    erb :show
  end
 
- get 'recipes/:id/edit' do
+ get '/recipes/:id/edit' do
    @recipe = Recipe.find_by_id(params[:id])
    erb :edit
  end
